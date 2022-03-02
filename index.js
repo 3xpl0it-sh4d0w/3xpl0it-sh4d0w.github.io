34d8192b5f6562b9683a60958540845d0df31bd7
3xpl0it.Sh4d0w <3xpl0it.sh4d0w@pm.me>
2022-03-02 22:36:03 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+document.write
+(
+  `
+  <title>3xpl0it Sh4d0w</tltle>
+  <link rel="stylesheet" href="./index.css">
+  
+  <script src="./assets/js/header.js"></script>
+  <script src="./assets/js/body.js"></script>
+  <script src="./assets/js/footer.js"></script>
+  `
+)
